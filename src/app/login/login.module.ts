abf1b3e9fe983321a8e665eb087f8899fe1d78d1
Liviu-Adrian Dodan <liviudodan@hotmail.com>
2022-01-04 11:57:19 +0100

    fix staged of branch merge

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,10 +1,10 @@
+import { LoginPageRoutingModule } from './login-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { LoginPageRoutingModule } from './login-routing.module';
 import { LoginPage } from './login.page';
 
 
